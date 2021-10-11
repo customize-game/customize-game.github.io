9beac9e655afbc61080a7a46fa79d768badf9b83
ShassBeleth <shassbeleth@gmail.com>
2021-10-12 00:28:55 +0900

    Revert "強引にCSSやJSの階層を変更"
    This reverts commit e0596b7380057f6f4a2380601a5c61d495390320.

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -25,7 +25,6 @@ export default defineConfig({
     rollupOptions: {
       external: ['@vitejs/plugin-legacy'],
     },
-    assetsDir: "introduction/assets"
   },
   define: {
     'process.env': {},
